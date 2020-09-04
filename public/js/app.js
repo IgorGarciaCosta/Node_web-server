33baf4bd843b2a0d5b1bfbfc5398b7f27b0bb496
IgorGarciaCosta <igonildo7@gmail.com>
2020-09-03 22:01:20 -0300

    including button and text field

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,20 @@
 console.log('Client side javascript file is loaded!')
+const weatherForm = document.querySelector('form')
+const typedSearch = document.querySelector('input')
 
-fetch('http://localhost:3000/Weather?address=boston').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault() //prevent the browser to refresh the page
+
+    const typedLocation = typedSearch.value
+
+    fetch('http://localhost:3000/Weather?address=' + typedLocation).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
     })
 })
